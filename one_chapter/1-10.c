6165ed9ea89dea076131bfa4a08307d7ef5e9925
renyifan <renyifan@iairfly.com>
2018-12-10 19:28:49 +0800

    [dev] 1-10 Use of '\'.

---
+++ one_chapter/1-10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char data;
+
+    while(1) {
+        data = getchar();
+        // 制表符在键盘上是tab 键
+        if (data == '\t')
+            printf("\\t");
+        // 退格键在键盘上是backspace 键
+        else if (data == '\b')
+            printf("\\b");
+        else if (data == '\\')
+            printf("\\");
+        else
+            putchar(data);
+    }
+    return 0;
+}
